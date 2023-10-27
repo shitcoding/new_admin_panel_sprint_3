100091d7c9fe113748fd8daff89a5beaed3da512
shitcoding <shitcoder@pm.me>
2023-10-28 00:02:29 +0300

    edit settings

--- postgres_to_es/settings.py
+++ postgres_to_es/settings.py
@@ -1,16 +1,20 @@
-from pydantic import Field, PostgresDsn
+from pydantic import PostgresDsn, AnyHttpUrl
 from pydantic_settings import BaseSettings
 
-STATE_KEY = 'last_movies_updated'
-UPDATE_INTERVAL = 5  # Interval in seconds between checks for updates of data
 
 
 class ETLSettings(BaseSettings):
     pg_dsn: PostgresDsn
+    pg_chunk_size: int
+    elk_dsn: AnyHttpUrl
+    elk_index: str
+    elk_chunk_size: int
+    update_interval: int
 
     class Config:
+        env_file = '.env'
         env_file_encoding = 'utf-8'
+        case_sensitive = False
         extra = 'ignore'
-
 
 etl_settings = ETLSettings()
