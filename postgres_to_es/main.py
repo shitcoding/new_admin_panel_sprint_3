f89cfeda3573b6624c53f374db04f61cb64efd96
shitcoding <shitcoder@pm.me>
2023-10-28 00:02:20 +0300

    add entrypoint for ETL service

--- postgres_to_es/main.py
+++ postgres_to_es/main.py
@@ -1,33 +1,18 @@
-from datetime import datetime
-from time import sleep
+from concurrent.futures import ThreadPoolExecutor
 
-import psycopg
-from etl.extractor.fetch_from_postgres import fetch
-from etl.loader.load import load_to_es
-from etl.transformer.transform import transform
-from etl.utils.logger import logger
-from psycopg import ServerCursor
-from psycopg.rows import dict_row
-from settings import STATE_KEY, UPDATE_INTERVAL, etl_settings
-from state.json_file_storage import JsonFileStorage
-from state.models import State
+from etl.etl import start_etl
+from etl.extractor.extract import (FilmworkExtractor, GenreExtractor,
+                                   PersonExtractor)
+from utils.logger import logger
+
+
+def main():
+    with ThreadPoolExecutor() as pool:
+        pool.submit(start_etl, FilmworkExtractor, 'filmworks_last_exported')
+        pool.submit(start_etl, GenreExtractor, 'genres_last_exported')
+        pool.submit(start_etl, PersonExtractor, 'persons_last_exported')
+        logger.info('Starting ETL process...')
+
 
 if __name__ == '__main__':
-    state = State(JsonFileStorage(logger=logger))
-
-    dsn = str(etl_settings.pg_dsn)
-
-    with psycopg.connect(dsn, row_factory=dict_row) as conn, ServerCursor(
-        conn, 'fetcher'
-    ) as cur:
-        saver_coro = load_to_es(state)
-        transformer_coro = transform(next_node=saver_coro)
-        fetcher_coro = fetch(cur, transformer_coro)
-
-        while True:
-            last_movies_updated = state.get_state(STATE_KEY)
-            logger.info('Starting ETL process for updates...')
-
-            fetcher_coro.send(state.get_state(STATE_KEY) or str(datetime.min))
-
-            sleep(UPDATE_INTERVAL)
+    main()
