b0a880e37e4cfd6876922da903a96458abd76720
shitcoding <shitcoder@pm.me>
2023-10-30 17:58:27 +0300

    implement rows transformation

--- postgres_to_es/etl/transformer/transform.py
+++ postgres_to_es/etl/transformer/transform.py
@@ -1,23 +1,24 @@
 from typing import Generator
+from models.models import Filmwork
 
-from models.models import Filmwork, Genre, Person
 from utils.logger import logger
 
 
 class FilmworkTransformer:
-    def __init__(self, next_node: Generator):
-        self.next_node = next_node
+    def __init__(self, loader: Generator):
+        self.loader = loader
 
     def transform(self):
-        next_node = self.next_node()
+        # Initialize next node generator
+        next_node = self.loader()
         next_node.send(None)
 
         try:
-            while dicts := (yield):
-                batch = []
-                for _dict in dicts:
-                    logger.info(f'Transforming entry: {_dict}')
-                    batch.append(_dict)
-                next_node.send(batch)
+            while True:
+                last_modified, modified_rows = (yield)
+                modified_rows: list[Filmwork]
+                for row in modified_rows:
+                    row.transform()
+                next_node.send((last_modified, modified_rows))
         except GeneratorExit:
-            logger.info('Finished transforming entries')
+            logger.debug('Finished transforming entries')
--- postgres_to_es/models/models.py
+++ postgres_to_es/models/models.py
@@ -1,38 +1,44 @@
-import uuid
 from datetime import datetime
 
 from pydantic import BaseModel
 
 
 class ModifiedRow(BaseModel):
-    id: uuid.UUID
+    id: str
     modified: datetime
 
 
 class Genre(BaseModel):
-    id: uuid.UUID
+    id: str
     name: str
-    modified: datetime
 
 
 class Person(BaseModel):
-    id: uuid.UUID
-    full_name: str
-    modified: datetime
+    id: str
+    name: str
 
 
 class Filmwork(BaseModel):
-    id: uuid.UUID
-    rating: str | float | None
+    id: str
+    rating: float | str | None
     title: str
     description: str | None
-    type: str
-    genres_names: list[str] | None
     genres: list[Genre] | None
-    directors_names: list[str] | None
     directors: list[Person] | None
-    actors_names: list[str] | None
     actors: list[Person] | None
-    writers_names: list[str] | None
     writers: list[Person] | None
-    modified: datetime
+    genres_names: list[str] | str | None = None
+    directors_names: list[str] | str | None = None
+    actors_names: list[str] | str | None = None
+    writers_names: list[str] | str | None = None
+
+    @staticmethod
+    def get_names(objects: list[Person] | list[Genre]):
+        return [obj.name for obj in objects]
+
+    def transform(self):
+        self.rating = float(self.rating) if self.rating else None
+        self.genres_names = self.get_names(self.genres)
+        self.directors_names = self.get_names(self.directors)
+        self.actors_names = self.get_names(self.actors)
+        self.writers_names = self.get_names(self.writers)
