c5cbf38bc1cfbc947341e8b9aa8c0d9a8fa416db
shitcoding <shitcoder@pm.me>
2023-11-01 05:01:39 +0300

    edit etl entrypoint

--- postgres_to_es/etl/etl.py
+++ postgres_to_es/etl/etl.py
@@ -1,34 +1,49 @@
+from contextlib import closing
+from datetime import datetime
 from time import sleep
 from typing import Type
 
-import psycopg
-from psycopg import ServerCursor
-from psycopg.rows import dict_row
+from psycopg2.extras import DictCursor
 
+from clients.elk import ElasticsearchClient
+from clients.postgres import PostgresClient
 from etl.extractor.extract import BaseExtractor
 from etl.loader.load import FilmworkLoader
 from etl.transformer.transform import FilmworkTransformer
-from settings import CHUNK_SIZE, UPDATE_INTERVAL, etl_settings
-from state.storage import JsonFileStorage
+from settings import etl_settings
 from state.state import State
+from state.storage import JsonFileStorage
+
+from utils.logger import logger
 
 
+@logger.catch
 def start_etl(extractor_class: Type[BaseExtractor], state_key: str):
-    state = State(JsonFileStorage(), state_key)
-    pg_dsn = str(etl_settings.pg_dsn)
 
-    with psycopg.connect(
-        pg_dsn, row_factory=dict_row
-    ) as pg_conn, ServerCursor(pg_conn, 'etl_pg_cursor') as cur:
-        loader = FilmworkLoader(state=state)
-        transformer = FilmworkTransformer(next_node=loader.load_to_es)
-        extractor = extractor_class(
-            cur=cur,
+    pg_dsn = str(etl_settings.pg_dsn)
+    elk_dsn = str(etl_settings.elk_dsn)
+
+    with closing(
+        PostgresClient(pg_dsn, cursor_factory=DictCursor)
+    ) as pg_conn, closing(ElasticsearchClient(elk_dsn)) as elk_conn:
+        state = State(JsonFileStorage(), state_key)
+        if not state.state_key_exists():
+            state.set_state(str(datetime.min))
+
+        loader = FilmworkLoader(
             state=state,
-            chunk_size=CHUNK_SIZE,
-            next_node=transformer.transform,
+            elk_conn=elk_conn,
+            elk_index=etl_settings.elk_index,
+            elk_chunk_size=etl_settings.elk_chunk_size,
+        )
+        transformer = FilmworkTransformer(loader=loader.load_to_es)
+        extractor = extractor_class(
+            state=state,
+            pg_conn=pg_conn,
+            pg_chunk_size=etl_settings.pg_chunk_size,
+            transformer=transformer.transform,
         )
 
         while True:
             extractor.extract()
-            sleep(UPDATE_INTERVAL)
+            sleep(etl_settings.update_interval)
--- postgres_to_es/main.py
+++ postgres_to_es/main.py
@@ -1,12 +1,27 @@
 from concurrent.futures import ThreadPoolExecutor
+from contextlib import closing
+import json
 
+from clients.elk import ElasticsearchClient
 from etl.etl import start_etl
 from etl.extractor.extract import (FilmworkExtractor, GenreExtractor,
                                    PersonExtractor)
+from settings import etl_settings
 from utils.logger import logger
 
 
 def main():
+    elk_dsn = str(etl_settings.elk_dsn)
+    elk_index = etl_settings.elk_index
+
+    with closing(ElasticsearchClient(elk_dsn)) as elk_conn:
+        if not elk_conn.index_exists(elk_index):
+            logger.warning('Elasticsearch index {} does not exist, creating index...', elk_index)
+            with open('elk_index.json', 'r') as f:
+                data = json.load(f)
+                elk_conn.index_create(elk_index, body=data)
+            logger.warning('Elasticsearch index {} has been created', elk_index)
+
     with ThreadPoolExecutor() as pool:
         pool.submit(start_etl, FilmworkExtractor, 'filmworks_last_exported')
         pool.submit(start_etl, GenreExtractor, 'genres_last_exported')
